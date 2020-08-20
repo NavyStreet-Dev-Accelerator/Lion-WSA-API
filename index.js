7c8d2fffdd115d8cee8d1c9907b25925b01bd549
Hannah Livnat <hannahlivnat@gmail.com>
2020-08-20 10:59:56 -0700

    made getWebScrape lambda friendly

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const url =
-    "https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States";
-const searchWord = "Lincoln";
-
-const getWebScrape = async () => {
-  try {
-    const response = await axios.get(url);
-    const $ = await cheerio.load(response.data);
-    const body = $('body').text().toLowerCase();
-    const searchTerm = searchWord.toLowerCase();
-    //console.log(body);
-    //console.log(typeof body);
+const searchThoughTerms = (body, searchTerm) => {
     let matchingTerms = 0;
 
     body.split(" ").map((word) => {
-      if (word.match(searchTerm)) {
-        matchingTerms += 1;
-      }
-    })
+        if (word.match(searchTerm)) {
+            matchingTerms += 1;
+        }
+    });
 
-    console.log(`There are ${matchingTerms} instances of ${searchTerm}`);
+    return matchingTerms;
+};
+
+const getWebScrape = async (url, query) => {
+    try {
+        const corsURL = "https://cors-anywhere.herokuapp.com/" + url;
+        const response = await axios.get(corsURL);
+        const $ = await cheerio.load(response.data);
+        const body = $("body").text().toLowerCase();
+        const searchTerm = query.toLowerCase();
+        searchThoughTerms(body, searchTerm);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 
-  } catch (error) {
-    console.log(error);
-  }
-
+const main = async(event) =>{
+  const {queryStringParameters: {
+    url, query
+  }} = event  
+  getWebScrape(url, query)
 }
 
-getWebScrape();
+exports.handler = main;
