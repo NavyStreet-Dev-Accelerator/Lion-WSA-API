cb6f3277559dea9d10cd30de7cc1df282ac867d1
Hannah Livnat <hannahlivnat@gmail.com>
2020-08-26 14:22:18 -0700

    now able to search for word and phrase matches using regex matchAll

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 exports.handler = async (event) => {
+
     const { queryStringParameters } = event;
+
     try {
         const res = await axios.get(queryStringParameters.url);
         const $ = await cheerio.load(res.data);
         const body = $("body").text().toLowerCase();
 
-        let matchingTerms = 0;
         const searchTerm = queryStringParameters.query.toLowerCase();
-        body.split(" ").map((word) => {
-            if (word.match(searchTerm)) {
-                matchingTerms += 1;
-            }
-        });
+        const matches = Array.from(body.matchAll(`${searchTerm}`));
+        const matchesLength = matches.length;
 
         return {
             statusCode: 200,
-            body: matchingTerms,
+            body: matchesLength
         };
     } catch (e) {
         console.log(e);
         return {
             statusCode: 400,
-            body: e.message,
+            body: e.message
         };
     }
 };
